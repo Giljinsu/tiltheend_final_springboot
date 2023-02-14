b4ebc874b0ebc4513234bb115339a57d507c8bed
Giljinsu <therin234@gmail.com>
2023-02-14 14:26:55 +0900

    first commit

---
+++ src/main/java/com/final_project/tiltheend_final_springboot/TiltheendFinalSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.final_project.tiltheend_final_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiltheendFinalSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiltheendFinalSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/final_project/tiltheend_final_springboot/TiltheendFinalSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.final_project.tiltheend_final_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiltheendFinalSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
