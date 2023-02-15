ae70567b8d38c81738b3ff3c76006727ff782a40
Giljinsu <therin234@gmail.com>
2023-02-15 11:04:06 +0900

    11

--- src/main/java/com/final_project/tiltheend_final_springboot/controller/ByPassController.java
+++ src/main/java/com/final_project/tiltheend_final_springboot/controller/ByPassController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ByPassController {
-<<<<<<< HEAD
     @RequestMapping(value = {"/index"})
     public void main() {
         int i = 1;
