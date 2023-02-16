5782026072b8670e8a53f11ce8806faff239b1f2
yongbeom76 <yongbeom76@gmail.com>
2023-02-16 14:18:24 +0900

    ShopService.java ShopMapper.xml

--- src/main/java/com/final_project/tiltheend_final_springboot/service/ShopService.java
+++ src/main/java/com/final_project/tiltheend_final_springboot/service/ShopService.java
@@ -11,10 +11,28 @@ public class ShopService {
   @Autowired
   CommonDao commonDao;
 
-  public Object getShop(Object dataMap) {
-    String sqlMapId = "shop.select";
+  public Object getProductList(Object dataMap) {
+    String sqlMapId = "shop.selectAll";
     Object result = commonDao.select(sqlMapId, dataMap);
+    return result;
+  }
 
+  public Object getBestsellingProductList(Object dataMap) {
+    String sqlMapId = "shop.selectBestsellingProductList";
+    Object result = commonDao.select(sqlMapId, dataMap);
+    return result;
+  }
+
+  public Object insertProduct(Object dataMap) {
+    String sqlMapId = "shop.insertProduct";
+    Object result = commonDao.insert(sqlMapId, dataMap);
+    return result;
+  }
+
+  public Object updateProduct(Object dataMap) {
+    String sqlMapId = "shop.updateProduct";
+    Object result = commonDao.update(sqlMapId, dataMap);
+    return result;
   }
 
 }
