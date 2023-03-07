9191c19da07983599b842ad4c6ebe36a90f2442e
yongbeom76 <yongbeom76@gmail.com>
2023-03-07 18:41:30 +0900

    commonutil test

--- src/main/java/com/final_project/tiltheend_final_springboot/utils/CommonUtils.java
+++ src/main/java/com/final_project/tiltheend_final_springboot/utils/CommonUtils.java
@@ -20,15 +20,16 @@ public class CommonUtils {
         return uuid.toString();
     }
 
-    // 절대 경로는 기존 작업 디렉토리에 관계없이 파일 또는 폴더의 위치를 ..으로 설명하는 경로입니다. 사실 그것은 루트 디렉토리에 상대적입니다. 
-    // 상대 경로는 현재 작업 디렉토리를 기준으로 파일 또는 폴더의 위치를 ​​설명하는 경로입니다. 
+    // 절대 경로는 기존 작업 디렉토리에 관계없이 파일 또는 폴더의 위치를 ..으로 설명하는 경로입니다. 사실 그것은 루트 디렉토리에
+    // 상대적입니다.
+    // 상대 경로는 현재 작업 디렉토리를 기준으로 파일 또는 폴더의 위치를 ​​설명하는 경로입니다.
     public String getAbsolutePath() {
         String relativePath = "src\\main\\resources\\static\\files\\";
-        String relativePathWithSeparator = relativePath.replace("/", File.separator);
-        String absolutePath = new File(relativePathWithSeparator).getAbsolutePath()+File.separator;
+        // String relativePathWithSeparator = relativePath.replace("/", File.separator);
+        String relativePathWithSeparator = relativePath.replace("\\", File.separator);
+        String absolutePath = new File(relativePathWithSeparator).getAbsolutePath() + File.separator;
         return absolutePath;
     }
-
 
     public List createFiles(MultipartHttpServletRequest multipartHttpServletRequest, Map params) {
         Map attachFile;
@@ -39,14 +40,14 @@ public class CommonUtils {
         String storePath = absolutePath + physicalFileName + File.separator;
         File newPath = new File(storePath);
         newPath.mkdir();
-        int fileOrder=0;
-        while(fileNames.hasNext()) {
+        int fileOrder = 0;
+        while (fileNames.hasNext()) {
             String fileName = fileNames.next();
             MultipartFile multipartFile = multipartHttpServletRequest.getFile(fileName);
             String originalFileName = multipartFile.getOriginalFilename();
 
-            if(originalFileName != null && multipartFile.getSize() !=0) {
-                String storeFilePath = storePath+originalFileName;
+            if (originalFileName != null && multipartFile.getSize() != 0) {
+                String storeFilePath = storePath + originalFileName;
                 try {
                     multipartFile.transferTo(new File(storeFilePath));
 
@@ -58,7 +59,7 @@ public class CommonUtils {
                     attachFile.put("REGISTER_SEQ", params.get("REGISTER_SEQ"));
                     attachFile.put("MODIFIER_SEQ", params.get("MODIFIER_SEQ"));
                     attachFile.put("FILE_ORDER", ++fileOrder);
-                
+
                     attachfiles.add(attachFile);
                 } catch (IllegalStateException e) {
                     // TODO Auto-generated catch block
@@ -74,38 +75,37 @@ public class CommonUtils {
 
     public void deleteFile(Object path) {
         String absolutePath = getAbsolutePath();
-        String physicalfile_name = (String)path;
-        String storePath = absolutePath+physicalfile_name+File.separator;
-        File file = new File(storePath); //폴더
+        String physicalfile_name = (String) path;
+        String storePath = absolutePath + physicalfile_name + File.separator;
+        File file = new File(storePath); // 폴더
         // 파일이 존재하면
-        if(file.exists()) {
-            if(file.isDirectory()) { //폴더이면
-                File[] files= file.listFiles(); //안에 내용물 리스트로저장
-                for(int i=0; i<files.length ; i++) {
-                    files[i].delete(); //하나씩 지움
+        if (file.exists()) {
+            if (file.isDirectory()) { // 폴더이면
+                File[] files = file.listFiles(); // 안에 내용물 리스트로저장
+                for (int i = 0; i < files.length; i++) {
+                    files[i].delete(); // 하나씩 지움
                 }
             }
-            file.delete(); //마지막으로 폴더 지우
+            file.delete(); // 마지막으로 폴더 지우
         }
     }
 
-
-    public List updateFiles(MultipartHttpServletRequest multipartHttpServletRequest, String physicalFileName, 
+    public List updateFiles(MultipartHttpServletRequest multipartHttpServletRequest, String physicalFileName,
             List originalFileName) { // 새로 들어온 파일 이름들을 저장함
         // 파일 업데이트 = 삭제 후 저장
         Iterator<String> fileNames = multipartHttpServletRequest.getFileNames();
         String absolutePath = this.getAbsolutePath();
         String storePath = absolutePath + physicalFileName + File.separator;
-        File file = new File(storePath); //폴더
-        
-        //삭제
-        if(file.exists()) {
-            if(file.isDirectory()) { //폴더이면
-                File[] files= file.listFiles(); //안에 내용물 리스트로저장
-                for(int i=0; i<files.length ; i++) {
-                    for(int j=0; j<originalFileName.size(); j++){
-                        if(files[i].getName().equals(originalFileName.get(j))) {// 기존의 있던 파일 삭제
-                            files[i].delete(); 
+        File file = new File(storePath); // 폴더
+
+        // 삭제
+        if (file.exists()) {
+            if (file.isDirectory()) { // 폴더이면
+                File[] files = file.listFiles(); // 안에 내용물 리스트로저장
+                for (int i = 0; i < files.length; i++) {
+                    for (int j = 0; j < originalFileName.size(); j++) {
+                        if (files[i].getName().equals(originalFileName.get(j))) {// 기존의 있던 파일 삭제
+                            files[i].delete();
                         }
                     }
                 }
@@ -113,14 +113,14 @@ public class CommonUtils {
         }
 
         originalFileName = new ArrayList<>();
-        //저장
-        while(fileNames.hasNext()) {
+        // 저장
+        while (fileNames.hasNext()) {
             String fileName = fileNames.next();
             MultipartFile multipartFile = multipartHttpServletRequest.getFile(fileName);
             String currentFileName = multipartFile.getOriginalFilename();
-            if(currentFileName != null && multipartFile.getSize() !=0) {
+            if (currentFileName != null && multipartFile.getSize() != 0) {
                 originalFileName.add(multipartFile.getOriginalFilename());
-                String storeFilePath = storePath+currentFileName;
+                String storeFilePath = storePath + currentFileName;
                 try {
                     multipartFile.transferTo(new File(storeFilePath));
 
@@ -136,29 +136,30 @@ public class CommonUtils {
         return originalFileName;
     }
 
-    // public void updateFiles(MultipartHttpServletRequest multipartHttpServletRequest, Map params) {
-    //     Map attachFile;
-    //     List attachfiles = new ArrayList<>();
-    //     Iterator<String> fileNames = multipartHttpServletRequest.getFileNames();
-    //     String absolutePath = getAbsolutePath();
-    //     String physicalFileName = (String) params.get("PHYSICALFILE_NAME");
-    //     String storePath = absolutePath + physicalFileName + File.separator;
-    //     File file = new File(storePath);
-    //     // newPath.mkdir();
-    //     while(fileNames.hasNext()) {
-    //         String fileName = fileNames.next();
-    //         MultipartFile multipartFile = multipartHttpServletRequest.getFile(fileName);
-    //         String originalFileName = multipartFile.getOriginalFilename();
-    //         if(originalFileName != null) {
-    //             if(file.exists()) {
-    //                 if(file.isDirectory()) { //폴더이면
-    //                     File[] files= file.listFiles(); //안에 내용물 리스트로저장
-    //                     for(int i=0; i<files.length ; i++) {
-    //                         // if(files[i]==originalFileName); //하나씩 지움
-    //                     }
-    //                 }
-    //         }
-    //     }
-    //     // return null;
+    // public void updateFiles(MultipartHttpServletRequest
+    // multipartHttpServletRequest, Map params) {
+    // Map attachFile;
+    // List attachfiles = new ArrayList<>();
+    // Iterator<String> fileNames = multipartHttpServletRequest.getFileNames();
+    // String absolutePath = getAbsolutePath();
+    // String physicalFileName = (String) params.get("PHYSICALFILE_NAME");
+    // String storePath = absolutePath + physicalFileName + File.separator;
+    // File file = new File(storePath);
+    // // newPath.mkdir();
+    // while(fileNames.hasNext()) {
+    // String fileName = fileNames.next();
+    // MultipartFile multipartFile = multipartHttpServletRequest.getFile(fileName);
+    // String originalFileName = multipartFile.getOriginalFilename();
+    // if(originalFileName != null) {
+    // if(file.exists()) {
+    // if(file.isDirectory()) { //폴더이면
+    // File[] files= file.listFiles(); //안에 내용물 리스트로저장
+    // for(int i=0; i<files.length ; i++) {
+    // // if(files[i]==originalFileName); //하나씩 지움
+    // }
+    // }
+    // }
+    // }
+    // // return null;
     // }}
 }
