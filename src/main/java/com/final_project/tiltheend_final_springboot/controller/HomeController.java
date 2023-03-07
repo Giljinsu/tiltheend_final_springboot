348b4b7e6c173d7e3c65c51ffefaaa3b6ab8ac4e
yongbeom76 <yongbeom76@gmail.com>
2023-03-07 10:49:44 +0900

    top ranking 수정중

--- src/main/java/com/final_project/tiltheend_final_springboot/controller/HomeController.java
+++ src/main/java/com/final_project/tiltheend_final_springboot/controller/HomeController.java
@@ -1,12 +1,24 @@
 package com.final_project.tiltheend_final_springboot.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.final_project.tiltheend_final_springboot.service.ShopService;
 
 @Controller
 public class HomeController {
-    @RequestMapping(value = {"/",""})
-    public String bypass() {
-        return "/index";
+
+    @Autowired
+    ShopService shopService;
+
+    @RequestMapping(value = { "/", "" })
+    public ModelAndView bypass(ModelAndView modelAndView) {
+
+        Object resultMapBestProduct = shopService.getBestsellingProductList();
+        modelAndView.addObject("resultMapBestProduct", resultMapBestProduct);
+        modelAndView.setViewName("/index");
+        return modelAndView;
     }
 }
