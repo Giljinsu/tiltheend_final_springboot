1d266158ac2d2f13f5361a2b9d1847f80129e953
yongbeom76 <yongbeom76@gmail.com>
2023-03-08 14:23:44 +0900

    images

--- src/main/java/com/final_project/tiltheend_final_springboot/configuration/WebMvcConfig.java
+++ src/main/java/com/final_project/tiltheend_final_springboot/configuration/WebMvcConfig.java
@@ -1,5 +1,7 @@
 package com.final_project.tiltheend_final_springboot.configuration;
 
+import java.io.File;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,5 +13,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
     registry.addResourceHandler("/files/**")
         .addResourceLocations("classpath:/static/files/");
+  }
+
+  public String getAbsolutePath() {
+    String relativePath = "src\\main\\resources\\static\\files\\";
+    // String relativePath = "images\\";
+    String relativePathWithSeparator = relativePath.replace("\\", File.separator);
+    String absolutePath = new File(relativePathWithSeparator).getAbsolutePath() + File.separator;
+    return absolutePath;
   }
 }
